1b40d31c3e4426c77d183940e400bb433275519d
Keyjekk <108344123+Keyjekk@users.noreply.github.com>
2023-03-10 18:23:40 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1678452549,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/9patch.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/loading-sheet0.png",
+		"images/tiledbackground3.png",
+		"images/tiledbackground4.png",
+		"images/loaded-sheet0.png",
+		"images/9patch2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
